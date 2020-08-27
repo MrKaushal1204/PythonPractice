3ae3402594f34d0bfb20d13cbad99a42534859e5
Mrkaushal1204 <kaushalkishor308532@gmail.com>
2020-08-28 00:27:15 +0530

    Emoji comment is added

--- EscapeSequences.py
+++ EscapeSequences.py
@@ -3,4 +3,4 @@ print("These are /\\/\\/\\/\\/\\ mountains")
 print("he is \t awesome")
 print("\\\" \\n \\t \\'")
 print(r"Line a \n Line b")
-print("\U0001F602")
+print("\U0001F602") #emoji codes for printing emojies
