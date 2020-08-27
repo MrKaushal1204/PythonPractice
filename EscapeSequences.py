a210a8bb103c1f9def9b690829e9ed39d6c3c228
Mrkaushal1204 <kaushalkishor308532@gmail.com>
2020-08-28 00:12:11 +0530

    Committing EscaprSequence file

---
+++ EscapeSequences.py
@@ -0,0 +1,6 @@
+print("This is double backslash \\\\")
+print("These are /\\/\\/\\/\\/\\ mountains")
+print("he is \t awesome")
+print("\\\" \\n \\t \\'")
+print(r"Line a \n Line b")
+print("\U0001F602")
